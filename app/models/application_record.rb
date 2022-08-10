dd2208472b3b37774039ed46eb91aab1649efe5a
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-10 16:35:30 -0500

    Fix models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -9,17 +9,17 @@ class User < ApplicationRecord
 end
 
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
 end
 
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 end
 
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 end
