192f71849a9d4f41a5be3d4d27a787e0ec7f77ed
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-10 21:28:30 -0500

    Add increment to posts count

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+  after_save :increment_user_post_count
+
+  def increment_user_post_count
+    author.posts_counter.nil? ? author.update(posts_counter: 1) : author.increment!(:posts_counter)
+  end
 end
