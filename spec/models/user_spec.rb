2f5568c92e6b0e6ade20d35d25ab73bd35ad3952
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-11 11:36:06 -0500

    Add sample test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
-class UserTest < ActiveSupport::TestCase
+RSpec.describe User, type: :model do
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
+  before { subject.save }
+  
+  it "should have a name" do
+    expect(subject.name).to eq('Tom')
+  end
 end
