ef396aa3fd779ce1cb4edf908c460b0bf3a93c5f
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-11 12:55:15 -0500

    Add user properties tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,22 @@ RSpec.describe User, type: :model do
 
   before { subject.save }
   
-  it "should have a name" do
+  it "should have a name of type string" do
     expect(subject.name).to eq('Tom')
+    expect(subject.name.class).to eq(String)
+  end
+  
+  it "should have an id of type integer" do
+    expect(subject.id.class).to eq(Integer)
+  end
+  
+  it "should have a photo link of type string" do
+    expect(subject.photo).to eq('https://unsplash.com/photos/F_-0BxGuVvo')
+    expect(subject.photo.class).to eq(String)
+  end
+  
+  it "should have a bio of type string" do
+    expect(subject.bio).to eq('Teacher from Mexico.')
+    expect(subject.bio.class).to eq(String)
   end
 end
