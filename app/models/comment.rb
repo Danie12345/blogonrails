607732ef0b9323c8ef5016f083d3c421083e786c
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-10 21:53:18 -0500

    Add increment comments count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,12 @@ require_relative 'application_record'
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  after_save :increment_post_comment_count
+
+  private
+
+  def increment_post_comment_count
+    post.comments_counter.nil? ? post.update(comments_counter: 1) : post.increment!(:comments_counter)
+  end
 end
