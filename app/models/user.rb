f54954135acdb6150f622281053bd29b559d5670
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-11 14:55:40 -0500

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
 
-  scope :most_recent_posts, ->(id) { from_user(id).posts.order(created_at: :desc).limit(3) }
+  validates :name, presence: true
+  validates :posts_counter, comparison { greater_than_or_equal_to: 0}
 
-  private
+  scope :most_recent_posts, ->(id) { from_user(id).posts.order(created_at: :desc).limit(3) }
   
+  private
+
   scope :from_user, ->(id) { where(id:)[0] }
 end
