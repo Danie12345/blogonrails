ca5097678200bdb1418156bc84e5f15712b59ea5
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-10 21:59:53 -0500

    Add increment post like counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,12 @@ require_relative 'application_record'
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  after_save :increment_post_like_count
+
+  private
+
+  def increment_post_like_count
+    post.likes_counter.nil? ? post.update(likes_counter: 1) : post.increment!(:likes_counter)
+  end
 end
