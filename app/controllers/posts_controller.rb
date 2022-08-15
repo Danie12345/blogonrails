1d857f3271532b33ac949eb1166d1a49bd2798de
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-15 15:44:18 -0500

    Add show method to posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    if Post.all.empty?
+      @post = 'no posts found at all!'
+    else
+      @post = Post.find(params[:id])
+    end
+  end
 end
