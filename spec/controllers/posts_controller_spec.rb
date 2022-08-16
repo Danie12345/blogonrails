92f02e7738d6390cfeb9293c3948b841ec98b533
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-16 13:56:00 -0500

    Create posts controller spec

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :request do
+  context "when client displays all posts from a user (#index)" do
+    it "should return a correct status response"
+    it "should render the correct template"
+    it "should contain the correct placeholder text in the body"
+  end
+
+  context "when client displays a single post from a user (#show)" do
+    it "should return a correct status response"
+    it "should render the correct template"
+    it "should contain the correct placeholder text in the body"
+  end
+end
