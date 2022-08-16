0b57f4aa6afbc72ee3139fa20612c7ceb5956b04
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-16 13:56:24 -0500

    Add /users/:id/posts tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
+  User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+  
   context "when client displays all posts from a user (#index)" do
-    it "should return a correct status response"
-    it "should render the correct template"
-    it "should contain the correct placeholder text in the body"
+    before(:each) { get "/users/864/posts" }
+
+    it "should return a correct (OK) status response" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "should render the correct template" do
+      expect(response).to render_template(:index)
+    end
+
+    it "should contain the correct placeholder text in the body" do
+      expect(response.body).to include("Hello posts index!")
+    end
   end
 
   context "when client displays a single post from a user (#show)" do
