96ce6f8d2d54a5bb154727845be6cc3396135fb6
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-16 13:23:20 -0500

    Add status response test to /users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :request do
   context "when client displays all users (#index)" do
-    it "should return a correct status response"
+    it "should return a correct (OK) status response" do
+      get "/users"
+      expect(response).to have_http_status(:ok)
+    end
     it "should render the correct template"
     it "should contain the correct placeholder text in the body"
   end
