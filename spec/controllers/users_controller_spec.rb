c8b3a65b9cc700bda3bc6b8a2cd40f5417f4a4b4
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-16 13:17:54 -0500

    Add users controller spec
    Add pending tests for controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :request do
+  context "when client displays all users (#index)" do
+    it "should return a correct status response"
+    it "should render the correct template"
+    it "should contain the correct placeholder text in the body"
+  end
+
+  context "when client displays a single user (#show)" do
+    it "should return a correct status response"
+    it "should render the correct template"
+    it "should contain the correct placeholder text in the body"
+  end
+end
