79c684394fa023770ca958269df2fa64b864f7ec
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-16 13:59:48 -0500

    Add /users/:id/posts/:id

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
-  User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+  user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
   
   context "when client displays all posts from a user (#index)" do
-    before(:each) { get "/users/864/posts" }
+    before(:each) { get "/users/#{user.id}/posts" }
 
     it "should return a correct (OK) status response" do
       expect(response).to have_http_status(:ok)
@@ -20,8 +20,20 @@ RSpec.describe PostsController, type: :request do
   end
 
   context "when client displays a single post from a user (#show)" do
-    it "should return a correct status response"
-    it "should render the correct template"
-    it "should contain the correct placeholder text in the body"
+    post = Post.create(author: user, title: 'Hello 1', text: 'This is my first post')
+
+    before(:each) { get "/users/#{user.id}/posts/#{post.id}" }
+
+    it "should return a correct (OK) status response" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "should render the correct template" do
+      expect(response).to render_template(:show)
+    end
+
+    it "should contain the correct placeholder text in the body" do
+      expect(response.body).to include("Hello individual post!")
+    end
   end
 end
