b7b3f5206190fe51c4b3e4d56880f514fa7477e1
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-17 11:42:50 -0500

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/users", to: "users#index"
+  root to: "users#index"
   resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show]
   end
