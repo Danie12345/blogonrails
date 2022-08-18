01dd382abb0444287de9d939e35c652bf638fd16
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-18 17:13:29 -0500

    Add create post route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "/users", to: "users#index"
   root to: "users#index"
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :new, :show]
+    resources :posts, only: [:index, :new, :create, :show]
   end
 end
