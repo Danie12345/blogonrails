df4e73d6cb28a266617abb35d55a8ff7f453311a
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-18 16:55:29 -0500

    Add new method to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,14 @@ class PostsController < ApplicationController
     @posts = Post.where(params[:id]).where(author_id: params[:user_id])
     @user = User.find(params[:user_id])
   end
+  
+  def new
+    @user = current_user
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: post } }
+    end
+  end
 
   def show
     @post = Post.find(params[:id])
