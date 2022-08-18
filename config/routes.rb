24f930c0c010b12639b36c0a48420d37f6141e4c
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-18 16:12:32 -0500

    Add the new post route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "/users", to: "users#index"
   root to: "users#index"
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :new, :show]
   end
 end
