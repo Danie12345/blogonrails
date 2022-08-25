3be2b74c8b920df430038f66e91998c8f7a57742
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-25 16:04:00 -0500

    Add comment counter decrement method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,10 +5,15 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_save :increment_post_comment_count
+  after_destroy :decrement_post_comment_count
 
   private
 
   def increment_post_comment_count
     post.increment!(:comments_counter)
+  end
+  
+  def decrement_post_comment_count
+    post.decrement!(:comments_counter)
   end
 end
