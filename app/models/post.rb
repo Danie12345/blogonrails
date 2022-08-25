c6b292794d91225cf549f4bcd49daae7c8c5081e
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-25 15:39:09 -0500

    Add dependent for likes and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ require_relative 'application_record'
 
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :likes
-  has_many :comments
+  has_many :likes, foreign_key: 'post_id', dependent: :delete_all
+  has_many :comments, foreign_key: 'post_id', dependent: :delete_all
 
   after_save :increment_user_post_count
 
