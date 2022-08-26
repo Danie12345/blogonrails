b945e50a1a687ecb2c3d058e071daea59167a5eb
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-25 23:44:53 -0500

    Default to json in routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
   end
 
-  namespace :api do
+  namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
       resources :users, only: [] do
         resources :posts, only: [:index] do
