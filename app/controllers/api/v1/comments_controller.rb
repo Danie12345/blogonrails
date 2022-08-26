62409ca3fa576d3557a11d7da87d81caae6d8e60
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-08-26 00:09:59 -0500

    Add create comments to controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,4 +5,18 @@ class Api::V1::CommentsController < ApplicationController
   def index
     render json: Comment.where(author_id: params[:user_id], post_id: params[:post_id]).all
   end
+
+  def create
+    @client = current_user
+    @post = Post.find(params[:post_id])
+    allparams = params.require(:comment).permit(:text)
+    allparams['author_id'] = @client.id
+    allparams['post_id'] = @post.id
+    comment = Comment.new(allparams)
+    if comment.save
+      redirect_to request.referrer
+    else
+      render :index
+    end
+  end
 end
