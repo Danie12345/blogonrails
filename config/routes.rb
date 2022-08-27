60a4cb342990c1b5aedd28ec11e5a5deaa88d948
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-08-27 07:57:55 -0500

    Run the install generator - rails g rswag:api:install - rails g rswag:ui:install - RAILS_ENV=test rails g rswag:specs:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  get "/users", to: "users#index"
-  root to: "users#index"
-  resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :new, :create, :show, :destroy] do
-      resources :comments, only: [:create, :destroy]
+  get '/users', to: 'users#index'
+  root to: 'users#index'
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index new create show destroy] do
+      resources :comments, only: %i[create destroy]
       resources :likes, only: [:create]
     end
   end
 
-  namespace :api, :defaults => {:format => :json} do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [] do
         resources :posts, only: [:index] do
-          resources :comments, only: [:index, :create]
+          resources :comments, only: %i[index create]
         end
       end
     end
